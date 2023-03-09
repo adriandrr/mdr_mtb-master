fb071aaef7101e700a74c47e56b6be362370759a
adriandrr <adriandoerr93@web.de>
2023-03-09 12:26:34 +0000

    test developing

--- .tests/setup-python-env.sh
+++ .tests/setup-python-env.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Create a new virtual environment
-python3 -m venv .tests/
+python3 -m venv .tests/venv
 
 # Activate the virtual environment
-source env/bin/activate
+source .tests/venv/bin/activate
 
 # Install the required packages
 pip install -r requirements.txt
